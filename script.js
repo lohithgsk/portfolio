52ac6ad6d822a8d3857fc8a6bc93454451d6e671
Lohith <69358508+lohithgsk@users.noreply.github.com>
2025-11-10 20:22:55 +0530

    updated exp

--- script.js
+++ script.js
@@ -16,7 +16,21 @@ function loadContent(section) {
         <h3>Experience</h3>
 
             <div class="exp-card">
-            <img class="exp-logo" src="pictures/samsung-logo.png" alt="Blue River Logo">
+            <img class="exp-logo" src="pictures/iitk.png" alt="IITK Logo">
+            <div class="exp-body">
+            <div class="exp-header">
+                <div class="exp-title">Indian Institute of Technology, Kanpur</div>
+                <div class="exp-dates">December 2025</div>
+            </div>
+            <div class="exp-role">Research Intern</div>
+            <ul class="exp-details">
+                <li>Working on Physics-Informed Neural Networks (PINNs) for data-driven modeling and analysis of complex 
+                turbulent flows.</li>
+            </div>
+            </div>
+
+            <div class="exp-card">
+            <img class="exp-logo" src="pictures/samsung-logo.png" alt="Samsung Logo">
             <div class="exp-body">
             <div class="exp-header">
                 <div class="exp-title">Samsung R&D Institute India-Bangalore</div>
@@ -32,7 +46,7 @@ function loadContent(section) {
             </div>
 
             <div class="exp-card">
-            <img class="exp-logo" src="pictures/fidelity-investments-logo.png" alt="Blue River Logo">
+            <img class="exp-logo" src="pictures/fidelity-investments-logo.png" alt="Fidelity Investments Logo">
             <div class="exp-body">
             <div class="exp-header">
                 <div class="exp-title">Fidelity Investments</div>
